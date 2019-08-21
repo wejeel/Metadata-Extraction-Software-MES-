91a1eb6dacfec9d2e448926117825cd76065524c
Weje Praise <wejepraise@yahoo.com>
2019-08-21 10:08:26 +0100

    initialize

---
+++ src/Forms/MetadataExtractionAndIndexingSoftwareMEIS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Forms;
+
+/**
+ *
+ * @author WEJE
+ */
+public class MetadataExtractionAndIndexingSoftwareMEIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginPage mu = new LoginPage();
+        mu.setVisible(true);
+    }
+    
+}
